a28bce249055273fde1808f82f170d2aca31c7e1
Andrey Starostin <andreystarostin.vio@gmail.com>
2024-02-26 13:51:21 +0300

    remove pagelayout

--- src/app/main/index.tsx
+++ src/app/main/index.tsx
@@ -1,7 +1,8 @@
 import PageLayout from "../../components/Page-Layout";
 
 function Main() {
-  return <PageLayout>Hello</PageLayout>;
+  //   return <PageLayout>Hello</PageLayout>;
+  return <div>Hello</div>;
 }
 
 export default Main;
