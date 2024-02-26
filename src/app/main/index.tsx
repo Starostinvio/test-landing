5c56a6718e304c830fce7c683b487033d184e7c9
Andrey Starostin <andreystarostin.vio@gmail.com>
2024-02-26 14:03:13 +0300

    remove BrowserRouter

--- src/app/main/index.tsx
+++ src/app/main/index.tsx
@@ -2,7 +2,7 @@ import PageLayout from "../../components/Page-Layout";
 
 function Main() {
   //   return <PageLayout>Hello new</PageLayout>;
-  return <div>Not PageLayout</div>;
+  return <div>Not BrowserRouter</div>;
 }
 
 export default Main;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/index";
 import { BrowserRouter } from "react-router-dom";
+import Main from "./app/main";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter> */}
+    {/* <App /> */}
+    {/* </BrowserRouter> */}
+    <Main />
   </React.StrictMode>
 );
